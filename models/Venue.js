60cb8aa9a76c723a8fc0258185b1b3cc4b1f5d9e
Jamie Newman <jamie.newman@gmail.com>
2016-03-17 21:51:56 +0000

    New service

--- models/Venue.js
+++ models/Venue.js
@@ -59,8 +59,8 @@ Venue.add({
         childrensMenu: { type: Boolean, label: 'Has a childrens menu?' },
         wheelAccessible: { type: Boolean, label: 'Pram/Wheelchair accessible?' },
         suitableForMany: { type: Boolean, label: 'Suitable for twins/multiples?' },
-        doublePramFriendly: { type: Boolean, label: 'Double pram friendly?' }
-           
+        doublePramFriendly: { type: Boolean, label: 'Double pram friendly?' },
+        goodFood: { type: Boolean, label: 'Good food for parents?' }
     },
     prices: {
         adult: { type: Types.Money, label: 'Adult price amount (£)', currency: 'en-gb' },
