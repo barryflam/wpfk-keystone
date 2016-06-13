078478ab48a71d916112a5c565fd8a4cfef2e198
Jamie Newman <jamie.newman@gmail.com>
2016-06-13 23:00:22 +0100

    Homepage styling

--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,7 @@ exports = module.exports = function(app) {
 	
 	// Views
 	app.get('/', routes.views.index);
+	app.get('/home', routes.views.home);    
 	app.all('/recommend', routes.views.recommend);
 	
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
